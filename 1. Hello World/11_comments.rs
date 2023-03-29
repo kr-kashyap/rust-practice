f8451070f53d0eabe753d2b55b8f26538863b3fd
Kashyap Nirmal <kashyap.n@knackroot.com>
2023-03-29 19:19:15 +0530

    Basic rust codes.

---
+++ 1. Hello World/11_comments.rs
@@ -0,0 +1,5 @@
+fn main() {
+	println!("// This is used for line comments.");
+	println!("/*...*/ This is used for block comments.");
+	println!("/// This is used for documentation comment.");
+}
---
+++ 1. Hello World/hello.rs
@@ -0,0 +1,6 @@
+// This is my first Rust code.
+
+fn main() {
+	let name = "Kashyap";
+	println!("Hello {}",name);
+}
